0634129872b20fe3e9b65282b177cf900bc01cf8
Puneeth V <puneethvijayakumar333@gmail.com>
2025-12-25 13:54:06 +0530

    frontend--ready

--- web page/frontend/src/layouts/Navbar.jsx
+++ web page/frontend/src/layouts/Navbar.jsx
@@ -59,16 +59,18 @@ const Navbar = ({ isAbout }) => {
       ref={navRef}
       className="bg-transparent flex items-center justify-between px-6 py-4 backdrop-blur-md sticky top-0 z-50"
     >
-      {/* Logo */}
+      
       <div className="logo transition-transform duration-300 hover:-translate-y-1 hover:scale-105 cursor-pointer">
-        <img
+        <a href="/">
+          <img
           src="https://res.cloudinary.com/deymewscv/image/upload/v1760798310/make_the_background_emesg0.png"
           alt="Vima Tech Logo"
           className="h-20 rounded-2xl shadow-md"
         />
+        </a>
       </div>
 
-      {/* Nav Links */}
+      
       <div className="flex items-center space-x-6 text-sky-100 font-medium text-lg">
         {!isAbout && (
           <a
